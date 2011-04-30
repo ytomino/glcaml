1ce9fd729caff1b3ccc49fb3c98397ca92fa5e4e
yt <demoonlit@panathenaia.halfmoon.jp>
2011-04-30 12:04:20 +0900

    Update for SDL_image 1.2.10

--- lib/sdl_image.ml
+++ lib/sdl_image.ml
@@ -1,4 +1,10 @@
 open Sdl;;
 
+external linked_version: unit -> int * int * int = "sdlimagestub_linked_version";;
+
+type init_flags = INIT_JPG | INIT_PNG | INIT_TIF;;
+
+external init: init_flags list -> unit = "sdlimagestub_init";;
+external quit: unit -> unit = "sdlimagestub_quit";;
 external load: file: string -> Video.surface = "sdlimagestub_load";;
 external read_xpm_from_array: xpm: string array -> Video.surface = "sdlimagestub_read_xpm_from_array";;
--- lib/sdl_image_stub.c
+++ lib/sdl_image_stub.c
@@ -10,12 +10,11 @@
 #include "caml/fail.h"
 #include "caml/alloc.h"
 
-/* exception */
+/* utilities */
 
-static void raise_failure(void)
-{
-	raise_with_string(*caml_named_value("SDL_failure"), IMG_GetError());
-}
+#define is_not_nil Is_block
+#define hd(v) Field((v), 0)
+#define tl(v) Field((v), 1)
 
 /* surface type (unmanaged) */
 
@@ -24,7 +23,58 @@ static inline value Val_SDL_Surface(SDL_Surface *surface)
 	return (value)surface;
 }
 
+/* IMG_InitFlags */
+
+static int const initflags_table[] = {
+    IMG_INIT_JPG,
+    IMG_INIT_PNG,
+    IMG_INIT_TIF,
+};
+
+static inline int Initflags_val(value initflags)
+{
+	int result = 0;
+	while(is_not_nil(initflags)){
+		result |= initflags_table[Int_val(hd(initflags))];
+		initflags = tl(initflags);
+	}
+	return result;
+}
+
+/* exception */
+
+static void raise_failure(void)
+{
+	raise_with_string(*caml_named_value("SDL_failure"), IMG_GetError());
+}
+
 /* primitives */
+
+CAMLprim value sdlimagestub_linked_version(value unit)
+{
+	CAMLparam1(unit);
+	CAMLlocal1(result);
+	SDL_version const *version = IMG_Linked_Version();
+	result = alloc(3, 0);
+	Store_field(result, 0, Val_int(version->major));
+	Store_field(result, 1, Val_int(version->minor));
+	Store_field(result, 2, Val_int(version->patch));
+	CAMLreturn(result);
+}
+
+CAMLprim value sdlimagestub_init(value flags)
+{
+	CAMLparam1(flags);
+	IMG_Init(Initflags_val(flags));
+	CAMLreturn(Val_unit);
+}
+
+CAMLprim value sdlimagestub_quit(value unit)
+{
+	CAMLparam1(unit);
+	IMG_Quit();
+	CAMLreturn(Val_unit);
+}
 
 CAMLprim value sdlimagestub_load(value file)
 {
